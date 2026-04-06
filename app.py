d76fc557f3433dc869d2de62d1239a4b29c1feed
Meghan Cullen <Megcullen@uiowa.edu>
2026-04-06 10:47:59 -0500

    inital commit of project

---
+++ app.py
@@ -0,0 +1,22 @@
+
+# module 10 - Flask Application
+# Meghan Cullen 4/6/2026
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!!"
+
+@app.route("/meghan")
+def meghan():
+    x = 6
+    y = 15
+    z = x + y 
+    name = "Meghan"
+    return f"Hello {name}, the sum of {x} and {y} is {z}!"
+
+if __name__ == "__main__":
+      app.run(host="0.0.0.0", port=5002, debug=True)
