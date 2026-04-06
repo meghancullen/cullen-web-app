1db98fb6b07385a9aed5309129e8b5b97db69584
Meghan Cullen <Megcullen@uiowa.edu>
2026-04-06 14:04:40 -0500

    Flask web app creation and styling

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
 # Meghan Cullen 4/6/2026
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return render_template("index.html")
 
 @app.route("/meghan")
 def meghan():
@@ -16,7 +17,11 @@ def meghan():
     y = 15
     z = x + y 
     name = "Meghan"
-    return f"Hello {name}, the sum of {x} and {y} is {z}!"
+    return f"Hello {name}, the sum of {x} and {y} is {z}"
+    
+@app.route("/about")
+def about():
+     return render_template("about.html")
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
